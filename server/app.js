165273ebad23746c9419972be1c2a9f243778673
mashengguang <mashengguang@sunlands.com>
2024-08-28 19:47:19 +0800

    delete:session

--- server/app.js
+++ server/app.js
@@ -1,31 +1,14 @@
 const path = require('path')
 const Koa = require('koa')
-const convert = require('koa-convert')
 const views = require('koa-views')
 const koaStatic = require('koa-static')
 const bodyParser = require('koa-bodyparser')
 const koaLogger = require('koa-logger')
-const session = require('koa-session-minimal')
-const MysqlStore = require('koa-mysql-session')
 
 const config = require('./../config')
 const routers = require('./routers/index')
 
 const app = new Koa()
-
-// session存储配置
-const sessionMysqlConfig= {
-  user: config.database.USERNAME,
-  password: config.database.PASSWORD,
-  database: config.database.DATABASE,
-  host: config.database.HOST,
-}
-
-// 配置session中间件
-app.use(session({
-  key: 'USER_SID',
-  store: new MysqlStore(sessionMysqlConfig)
-}))
 
 // 配置控制台日志中间件
 app.use(koaLogger())
