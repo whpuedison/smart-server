4eaac610cd98f9a0789b5e4e4468bc3645998d64
mashengguang <mashengguang@sunlands.com>
2024-08-28 19:43:14 +0800

    feat:init smart server

---
+++ config.js
@@ -0,0 +1,14 @@
+const config = {
+
+  port: 3001,
+
+  database: {
+    DATABASE: 'mydatabase',
+    USERNAME: 'root',
+    PASSWORD: 'password',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+module.exports = config
