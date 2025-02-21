73479f0c6dd40208cf3cf40c17892059e20c3f28
mashengguang <mashengguang@sunlands.com>
2025-02-21 16:26:21 +0800

    feat:处理依赖问题

--- server/models/chat.js
+++ server/models/chat.js
@@ -1,19 +1,20 @@
-const Mock = require('mockjs')
+// const Mock = require('mockjs')
 
 module.exports = {
     async chatList () {
-        const result = Mock.mock({
-            ['chats|20']: [
-                {
-                    'id|+1': 0,
-                    'avatar': function() {
-                        return Mock.Random.image('100x100', Mock.Random.color(), '#000000', 'Avatar');
-                    }, // 生成头像图片的 URL，背景色为随机色
-                    'nickname': '@cname', // 中文名字
-                    'message': () => Mock.mock('@csentence(1, 20)')
-                }
-            ]
-        })
+        const result = []
+        //  Mock.mock({
+        //     ['chats|20']: [
+        //         {
+        //             'id|+1': 0,
+        //             'avatar': function() {
+        //                 return Mock.Random.image('100x100', Mock.Random.color(), '#000000', 'Avatar');
+        //             }, // 生成头像图片的 URL，背景色为随机色
+        //             'nickname': '@cname', // 中文名字
+        //             'message': () => Mock.mock('@csentence(1, 20)')
+        //         }
+        //     ]
+        // })
         return result.chats;
     }
 }
